2089f46a9bd1183e7d9a2546eae9a282d2e535f8
wolven531 <anthony.williams.cs@gmail.com>
2022-08-24 22:09:38 -0600

    Add custom handler for home page

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -10,11 +10,16 @@ interface CounterProps {
 
 export default function Counter(props: CounterProps) {
 	const [count, setCount] = useState(props.start);
+
 	return (
 		<div>
 			<p>{count}</p>
-			<Button onClick={() => setCount(count - 1)}>-1</Button>
-			<Button onClick={() => setCount(count + 1)}>+1</Button>
+			<Button onClick={() => setCount((oldCount) => oldCount - 1)}>
+				-1
+			</Button>
+			<Button onClick={() => setCount((oldCount) => oldCount + 1)}>
+				+1
+			</Button>
 		</div>
 	);
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -2,15 +2,29 @@
 import { h } from 'preact';
 import Counter from '../islands/Counter.tsx';
 
+import { Handlers } from '$fresh/server.ts';
+
+export const handler: Handlers = {
+	GET: async (req, ctx) => {
+		console.info('Home page got hit!');
+
+		const resp = await ctx.render();
+
+		resp.headers.set('X-Custom-Header', 'Anthony Williams wuz heer');
+
+		return resp;
+	},
+};
+
 export default function Home() {
 	return (
 		<div>
 			<img
-				src='/logo.svg'
-				height='100px'
 				alt='the fresh logo: a sliced lemon dripping with juice'
+				height='100px'
+				src='/logo.svg'
 			/>
-			<p>Hello.</p>
+			<p>Hello</p>
 			<Counter start={3} />
 		</div>
 	);
