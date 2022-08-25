21af245947f17638507f957a696b929073cee43c
wolven531 <anthony.williams.cs@gmail.com>
2022-08-24 21:54:40 -0600

    Update greet route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,17 +2,17 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from './routes/[name].tsx';
-import * as $1 from './routes/about.tsx';
-import * as $2 from './routes/api/joke.ts';
+import * as $0 from './routes/about.tsx';
+import * as $1 from './routes/api/joke.ts';
+import * as $2 from './routes/greet/[name].tsx';
 import * as $3 from './routes/index.tsx';
 import * as $$0 from './islands/Counter.tsx';
 
 const manifest = {
 	routes: {
-		'./routes/[name].tsx': $0,
-		'./routes/about.tsx': $1,
-		'./routes/api/joke.ts': $2,
+		'./routes/about.tsx': $0,
+		'./routes/api/joke.ts': $1,
+		'./routes/greet/[name].tsx': $2,
 		'./routes/index.tsx': $3,
 	},
 	islands: {
