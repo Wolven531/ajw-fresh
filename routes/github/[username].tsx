a30055929130470e89b6920538649a701a858651
wolven531 <anthony.williams.cs@gmail.com>
2022-08-25 23:33:16 -0600

    Add GitHub user lookup route

---
+++ routes/github/[username].tsx
@@ -0,0 +1,43 @@
+/** @jsx h */
+import type { Handlers, PageProps } from '$fresh/server.ts';
+import { h } from 'preact';
+
+interface IGithubUser {
+	avatar_url: string;
+	login: string;
+	name: string;
+}
+
+export const handler: Handlers<IGithubUser | null> = {
+	GET: async (_, ctx) => {
+		const { username } = ctx.params;
+
+		const resp = await fetch(`https://api.github.com/users/${username}`);
+
+		if (resp.status === 404) {
+			return ctx.render();
+		}
+
+		const user: IGithubUser = await resp.json();
+
+		return ctx.render(user);
+	},
+};
+
+export default function Page({ data }: PageProps<IGithubUser>) {
+	if (!data) {
+		return <h1>User not found</h1>;
+	}
+
+	return (
+		<div>
+			<img
+				height={64}
+				src={data.avatar_url}
+				width={64}
+			/>
+			<h1>{data.name}</h1>
+			<p>{data.login}</p>
+		</div>
+	);
+}
