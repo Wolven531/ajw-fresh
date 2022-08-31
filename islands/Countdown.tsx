89f51ed8d06323d7a4f5efd89e15d8e66ef52a4c
wolven531 <anthony.williams.cs@gmail.com>
2022-08-30 23:59:35 -0600

    Start Countdown island

---
+++ islands/Countdown.tsx
@@ -0,0 +1,39 @@
+/** @jsx h */
+import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+
+const timeFmt = new Intl.RelativeTimeFormat('en-US');
+
+// The target date is passed as a string instead of as a `Date`, because the
+// props to island components need to be JSON (de)serializable
+export default function Countdown(props: { targetTimestamp: string }) {
+	const target = new Date(props.targetTimestamp);
+
+	const [now, setNow] = useState(Date.now());
+
+	// Set up an interval to update the `now` date every second with the current
+	// date as long as the component is mounted
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setNow((previousNow) => {
+				if (previousNow > target) {
+					clearInterval(timer);
+				}
+
+				return Date.now();
+			});
+		}, 1000);
+		return () => clearInterval(timer);
+	}, [props.targetTimestamp]);
+
+	// If the target date has passed, we stop counting down
+	if (now > target) {
+		return <span>🎉</span>;
+	}
+
+	// Otherwise, we format the remaining time using `Intl.RelativeTimeFormat` and
+	// render it
+	const secondsLeft = Math.floor((target.getTime() - now.getTime()) / 1000);
+
+	return <span>{timeFmt.format(secondsLeft, 'seconds')}</span>;
+}
