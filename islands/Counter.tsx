4c9ff9f9c71250cd3d8c6f02133e0d62911bc61a
wolven531 <anthony.williams.cs@gmail.com>
2022-09-16 23:54:41 -0600

    Clean up exports

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,22 +1,33 @@
+import type { FunctionComponent } from 'preact';
 import { useState } from 'preact/hooks';
 import { Button } from '../components/Button.tsx';
 
-interface ICounterProps {
+export interface ICounterProps {
 	start: number;
 }
 
-export default function Counter(props: ICounterProps) {
+export const Counter: FunctionComponent<ICounterProps> = (
+	props: ICounterProps,
+) => {
 	const [count, setCount] = useState(props.start);
 
 	return (
 		<div>
 			<p>{count}</p>
-			<Button className='m-1 p-1 bg-red-300' onClick={() => setCount((oldCount) => oldCount - 1)}>
+			<Button
+				className='m-1 p-1 bg-red-300'
+				onClick={() => setCount((oldCount) => oldCount - 1)}
+			>
 				-1
 			</Button>
-			<Button className='m-1 p-1 bg-green-300' onClick={() => setCount((oldCount) => oldCount + 1)}>
+			<Button
+				className='m-1 p-1 bg-green-300'
+				onClick={() => setCount((oldCount) => oldCount + 1)}
+			>
 				+1
 			</Button>
 		</div>
 	);
-}
+};
+
+export default Counter;
