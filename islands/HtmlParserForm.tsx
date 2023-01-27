31e38eaeb2d2d153b0154631364ac37a75252186
wolven531 <anthony.williams.cs@gmail.com>
2023-01-27 00:07:03 -0800

    Start parsing file

--- islands/HtmlParserForm.tsx
+++ islands/HtmlParserForm.tsx
@@ -1,9 +1,25 @@
-import type { FunctionComponent } from 'preact';
+import type { FunctionComponent, JSX } from 'preact';
 import { useEffect } from 'preact/hooks';
 
 export const HtmlParserForm: FunctionComponent = () => {
-	const handleSubmit = () => {
+	const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = (evt) => {
+		evt.preventDefault();
+
 		console.info('About to parse file');
+		// console.info(evt);
+
+		const target = evt.target as HTMLFormElement;
+		const fileInput = target.querySelector('input[type=\'file\'');
+		const { files } = fileInput as HTMLInputElement;
+		const fileItem = files?.item(0);
+
+		// console.info(fileItem);
+
+		fileItem?.text().then((fileText) => {
+			// console.info(fileText);
+
+			parseInfo(fileText);
+		});
 	};
 
 	useEffect(() => {
@@ -20,6 +36,16 @@ export const HtmlParserForm: FunctionComponent = () => {
 			</form>
 		</article>
 	);
+};
+
+const parseInfo = (htmlText: string) => {
+	const parser = new DOMParser();
+
+	const doc = parser.parseFromString(htmlText, 'text/html');
+
+	const theads = Array.from(doc.querySelectorAll('table > thead'));
+
+	console.info(theads.map((thead) => thead.textContent?.trim()));
 };
 
 export default HtmlParserForm;
