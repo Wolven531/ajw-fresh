94c7bf731e3064dc4a7ab0328d40a59fb0dca9bf
wolven531 <anthony.williams.cs@gmail.com>
2023-01-29 00:31:39 -0800

    Add site meta tags

---
+++ routes/_app.tsx
@@ -0,0 +1,14 @@
+import { Head } from '$fresh/runtime.ts';
+import { AppProps } from '$fresh/server.ts';
+
+export default function App(props: AppProps) {
+	return (
+		<>
+			<Head>
+				<meta name='author' content='Anthony Williams' />
+				<meta name='description' content='Website by Anthony Williams, built using Fresh + Deno' />
+			</Head>
+			<props.Component />
+		</>
+	);
+}
