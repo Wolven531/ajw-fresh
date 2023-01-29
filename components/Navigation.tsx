577ab18d7dff6d22a20a80cd7e2513e6928c292e
wolven531 <anthony.williams.cs@gmail.com>
2023-01-28 19:30:10 -0800

    Create Navigation comp

---
+++ components/Navigation.tsx
@@ -0,0 +1,23 @@
+export interface INavLinkProps {
+	content: string;
+	href: string;
+}
+
+export function NavLink({ content, href }: INavLinkProps) {
+	return (
+		<a href={href} className='p-1 hover:bg-green-500 hover:text-white'>
+			{content}
+		</a>
+	);
+}
+
+export function Navigation() {
+	return (
+		<nav className='border-1 border-green-500 flex flex-row content-center justify-between p-1'>
+			<NavLink href='/' content='Home' />
+			<NavLink href='/parse' content='Parser' />
+			<NavLink href='/countdown' content='Countdown' />
+			<NavLink href='/about' content='About' />
+		</nav>
+	);
+}
