e2841de82d1e3955b6796ec118a9e93c8caf7ac5
wolven531 <anthony.williams.cs@gmail.com>
2023-01-31 22:26:40 -0800

    Add constants file

---
+++ constants.ts
@@ -0,0 +1,29 @@
+export const datetimeFmt = new Intl.DateTimeFormat(
+	'en-US',
+	{
+		day: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+		month: '2-digit',
+		year: '2-digit',
+		timeZone: 'America/Los_Angeles',
+	},
+);
+
+export const timeFmt = new Intl.RelativeTimeFormat('en-US');
+
+// Jokes courtesy of https://punsandoneliners.com/randomness/programmer-jokes/
+export const JOKES = [
+	'Why do Java developers often wear glasses? They can\'t C#.',
+	'A SQL query walks into a bar, goes up to two tables and says “can I join you?”',
+	'Wasn\'t hard to crack Forrest Gump\'s password. 1forrest1.',
+	'I love pressing the F5 key. It\'s refreshing.',
+	'Called IT support and a chap from Australia came to fix my network connection.  I asked “Do you come from a LAN down under?”',
+	'There are 10 types of people in the world. Those who understand binary and those who don\'t.',
+	'Why are assembly programmers often wet? They work below C level.',
+	'My favourite computer based band is the Black IPs.',
+	'What programme do you use to predict the music tastes of former US presidential candidates? An Al Gore Rhythm.',
+	'An SEO expert walked into a bar, pub, inn, tavern, hostelry, public house.',
+];
+
+export const NAMES = ['alice', 'bob', 'charlie', 'dave', 'eve', 'frank'].sort();
