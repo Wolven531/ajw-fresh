cbc5848fa6b2845784e025450405434b72362042
wolven531 <anthony.williams.cs@gmail.com>
2023-02-05 22:38:32 -0800

    Use ParsedStat

--- components/ParsedTable.tsx
+++ components/ParsedTable.tsx
@@ -1,4 +1,5 @@
 import type { IParsedTable } from 'types';
+import { ParsedStat } from './ParsedStat.tsx';
 
 export interface IParsedTableProps {
 	table: IParsedTable;
@@ -8,9 +9,32 @@ export const ParsedTable = ({ table }: IParsedTableProps) => {
 	return (
 		<section className='border-1 border-green-300'>
 			<h4 className='font-bold'>
-				{table.title} ({table.rows.length} rows)
+				{table.title}
 			</h4>
-			{table.columnTitles.map((col) => <p key={col}>{col}</p>)}
+			<ParsedStat
+				name='Total rows'
+				statFunc={(trans) => trans.length}
+				transactions={table.rows}
+			/>
+			<ParsedStat
+				name='Total credit'
+				statFunc={(trans) =>
+					trans.map(({ credit }) => credit).reduce(
+						(total, c) => total + c,
+						0,
+					)}
+				transactions={table.rows}
+			/>
+			<ParsedStat
+				name='Total debit'
+				statFunc={(trans) =>
+					trans.map(({ debit }) => debit).reduce(
+						(total, d) => total + d,
+						0,
+					)}
+				transactions={table.rows}
+			/>
+			{/* {table.columnTitles.map((col) => <p key={col}>{col}</p>)} */}
 		</section>
 	);
 };
