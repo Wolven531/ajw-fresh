6ed2236f7274b369d195aca966908de601c6199f
wolven531 <anthony.williams.cs@gmail.com>
2023-02-16 18:12:54 -0800

    Start render for others

--- components/ParsedTable.tsx
+++ components/ParsedTable.tsx
@@ -1,4 +1,4 @@
-import type { IParsedTable } from 'types';
+import type { IParsedTable, ITransaction } from 'types';
 import { ParsedStat } from './ParsedStat.tsx';
 import { StatGroup } from './StatGroup.tsx';
 
@@ -7,6 +7,20 @@ export interface IParsedTableProps {
 }
 
 export const ParsedTable = ({ table }: IParsedTableProps) => {
+	const credits: ITransaction[] = [];
+	const debits: ITransaction[] = [];
+	const other: ITransaction[] = [];
+
+	table.rows.forEach((t) => {
+		if (t.credit > 0 && t.debit === 0) {
+			credits.push(t);
+		} else if (t.debit > 0 && t.credit === 0) {
+			debits.push(t);
+		} else {
+			other.push(t);
+		}
+	});
+
 	return (
 		<section className='border-1 border-green-300'>
 			<h4 className='font-bold'>
@@ -22,17 +36,29 @@ export const ParsedTable = ({ table }: IParsedTableProps) => {
 				<StatGroup
 					className='border-green-500'
 					name='credit'
-					transactions={table.rows.filter(({ credit, debit }) =>
-						credit > 0 && debit === 0
-					)}
+					transactions={credits}
 				/>
 				<StatGroup
 					className='border-red-300'
 					name='debit'
-					transactions={table.rows.filter(({ credit, debit }) =>
-						debit > 0 && credit === 0
-					)}
+					transactions={debits}
 				/>
+				{other.length > 0 && (
+					<div className='border-2 border-yellow-800 col-span-2 my-1 py-1'>
+						<h3>Other ({other.length} rows)</h3>
+						{other.map((t) => (
+							<div
+								className='border-1 border-yellow-600 my-1'
+								key={t.description}
+							>
+								<p>Description - "{t.description}"</p>
+								<p>Symbol - "{t.symbol}"</p>
+								<p>Credit = {t.credit} Debit = {t.debit}</p>
+								<p>Quantity = {t.quantity} Price = {t.price}</p>
+							</div>
+						))}
+					</div>
+				)}
 			</div>
 		</section>
 	);
